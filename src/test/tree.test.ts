f506a7ed271d663bea51c8a9baa15c96fb5d6c0f
Safa Selim <safaselim.ss@gmail.com>
2022-03-01 17:36:39 +0300

    updated test

--- src/test/tree.test.ts
+++ src/test/tree.test.ts
@@ -7,5 +7,5 @@ test('Testing to result method: tree.result() should be equal to 2', () => {
 
 test('Testing to toString method: tree.toString() should be equal to "((7 + ((3 - 2) x 5)) ÷ 6)"', () => {
 
-  expect(tree.result()).toEqual(2);
+  expect(tree.toString()).toEqual("((7 + ((3 - 2) x 5)) ÷ 6)");
 });
